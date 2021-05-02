f6099b5f801e4f8e3e5e246ccf4a8d4769b671ba
starlitbebe123 <82298568+starlitbebe123@users.noreply.github.com>
2021-05-02 12:59:11 +0800

    程式

--- 2D_Rockman/Assets/Scripts/Car.cs
+++ 2D_Rockman/Assets/Scripts/Car.cs
@@ -2,11 +2,13 @@
 
 public class Car : MonoBehaviour
 {
+    #region 欄位
     //單行註解
     /*
      * 多行註解
      * 
      */
+
 
     //物件資料-欄位 Field: 儲存物件資料
     //欄位語法
@@ -35,7 +37,7 @@ public class Car : MonoBehaviour
     //範圍 Range(限制最小值最大值的拉條) 只能用數值類型(整數或浮點數)
     [Header("汽車CC數")]
     [Tooltip("這是汽車的CC數")]
-    [Range(1000,5000)]
+    [Range(1000, 5000)]
     public int cc = 2000;
     //省空間可以用逗號
     [Header("汽車重量"), Tooltip("這是汽車的重量"), Range(0.5f, 10)]
@@ -50,7 +52,7 @@ public class Car : MonoBehaviour
     public Color color;
     public Color color2 = Color.red;
     //自訂顏色(R,G,B,Alpha)
-    public Color color3 = new Color(0,12,12, 0.5f);
+    public Color color3 = new Color(0, 12, 12, 0.5f);
 
     //座標 二維 三維 四維 Vector2 Vector3 Vector4
     public Vector2 v2;
@@ -66,7 +68,7 @@ public class Car : MonoBehaviour
     public KeyCode key2 = KeyCode.A;
     public KeyCode key3 = KeyCode.Mouse0;//滑鼠左鍵 0, 右1, 滾輪2
     public KeyCode key4 = KeyCode.Joystick1Button0;
-     
+
     // 遊戲物件 與 元件
     // 遊戲物件 GameObject
     public GameObject obj1;
@@ -74,4 +76,120 @@ public class Car : MonoBehaviour
     //名稱去掉空格 
     public Transform tra; //可儲存任何包含Transform元件的物件
     public SpriteRenderer sprite;//可儲存任何包含Sprite Renderer元件的物件
+    #endregion
+
+    #region 事件
+    public int number = 1;
+    public bool test = false;
+    public string prop = "紅色藥水";
+    int space = 0;
+    //事件:在特定時間點會被執行的方法
+    // Unity 提供的事件: 開始、更新
+
+    //開始事件: 播放遊戲後執行一次
+    //應用:數值初始化，例如:遊戲一開始多少金幣或生命值等等
+    private void Start()
+    {
+        // print(任何資料) - 輸出資料到Console儀表板上
+        print("我是開始事件唷~");
+
+        //欄位存取
+        // 取得
+        //語法:欄位名稱
+        //字串串接: 字串 + 其他欄位
+        print("取得欄位" + number);
+
+        //存放
+        //語法: 欄位名稱 指定 值
+        //值必須與此欄位類型相同
+        test = true;
+        print("存放欄位後的結果:" + test);
+        prop = "藍色藥水";
+        print("存放到具名稱:" + prop);
+
+        //呼叫方法
+        //方法名稱();
+        Test();
+        space = Ten();
+
+        //傳回方法:
+        //傳回類型 名稱 = 傳回方法();
+        int t = Ten();
+        print("傳回方法的結果:" + t);
+        //呼叫方法要有相同數量的參數
+        //有預設值的參數為【選填式參數】，沒另外寫就會出現預設值，就像"灰塵特效"
+        //【選填式參數】只能寫在最右邊
+        //有填的話會以後加的為主;
+        Drive(200, "咻");
+        //冒號:指定參數
+        //有多個選填式參數
+        //以下舉例為"音效為【選填式參數】"
+        Drive(70, "閃電特效"); //錯誤-會把特效放在音效上
+        Drive(70, effect: "閃電音效"); //正確-指定特效參數
+    }
+    //更新事件執行時間點與次數: 開始事件後以每秒六十次執行 60FPS
+    //應用:監控玩家輸入與物件的持續行為，例如:玩家有沒有按按鈕或讓物件持續移動
+    private void Update()
+    {
+        print("我是更新");
+    }
+    #endregion
+
+    #region 方法 Unity Method
+    //方法:保存較複雜或演算法的程式區塊
+    //語法:
+    //修飾詞 傳回類型 名稱() {較複雜或演算法的程式區塊}
+    // void 無傳回:使用這個方法不會有傳回
+    //方法是需要被 【呼叫】 才會執行
+    /// <summary>
+    /// 我是一個測試方法
+    /// </summary>
+    ///<returns>整數十</returns>
+    private void Test()
+    {
+        print("我是測試方法。");
+    }
+    //如果不是無傳回void就要return
+
+    private int Ten()
+    {
+        return 10;
+    }
+
+    //一般的的方法
+    private void Drive50()
+    {
+        print("開車時數:" + 50);
+        print("開車音效");
+    }
+    private void Drive100()
+    {
+        print("開車時數:" + 100);
+        print("開車音效");
+    }
+    private void Drive300()
+    {
+        print("開車時數:" + 300);
+        print("開車音效");
+    }
+    //進階的:用參數解決 Parameter
+    //參數語法:類型 參數名稱
+    /// <summary>
+    /// 開車
+    /// </summary>
+    /// <param name="speed">開車的時速</param>
+    /// <param name="sound">開車的音效</param>
+    /// <param name="effect">開車的特效</param>
+    //有預設值的參數為【選填式參數】，沒另外寫就會出現預設值，就像"灰塵特效"
+    //【選填式參數】只能寫在最右邊
+    private void Drive(int speed, string sound = "咻~", string effect = "灰塵效果")
+    {
+        print("開車時數:" + speed);
+        print("開車音效:" + sound);
+        print("特效:" + effect);
+    }
+
+
+
+    #endregion
 }
