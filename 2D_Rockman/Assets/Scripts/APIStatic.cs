40b0f33ba3cd22c5252f3f6f390d360626c01526
starlitbebe123 <82298568+starlitbebe123@users.noreply.github.com>
2021-05-02 16:58:33 +0800

    API

--- 2D_Rockman/Assets/Scripts/APIStatic.cs
+++ 2D_Rockman/Assets/Scripts/APIStatic.cs
@@ -5,21 +5,59 @@ public class APIStatic : MonoBehaviour
 {
     //認是靜態API
     //包含關鍵字 static 就是靜態
-
+ 
     private void Start()
     {
-        //取得是抓出資料，存放是修改資料
+        #region 認識靜態屬性方法方法
+        //靜態取得是抓出資料，靜態存放是修改資料
 
         //屬性 欄位 要知道如何存取
+
+        //語法:
+        //類別名稱.靜態屬性名稱
 
         //練習取得靜態屬性 Static Properties
         //語法:
         //類別名稱.靜態屬性名稱
-        float r=Random.value;  //隨機.值
+        float r =Random.value;  //隨機.值
         print("隨機值:" + r);
 
         //練習存放靜態屬性 Static Properties
         //有顯示(Read Only)的屬性不能存放
+        //語法:
+        //類別名稱.靜態屬性名稱
         Cursor.visible = false;  //指標.可見度
+
+        //練習使用靜態方法 Static Methods
+        //語法:
+        //類別名稱.靜態屬性名稱
+        int attack = Random.Range(100, 300);
+        print("隨機攻擊力:" + attack);
+
+        float n = Mathf.Abs(-99.9f);
+        print("絕對值內容:" + n);
+
+        //靜態屬性Static Properties:直接跑一個數值
+        //靜態方法Static Methods:運算出一個值
+        #endregion
+        
+        //練習靜態屬性
+        //靜態取得
+        print("攝影機數量:" + Camera.allCamerasCount);
+        print("2D重力大小:" + Physics2D.gravity);
+        //靜態存放
+        Physics2D.gravity = new Vector2(0, -20);
+
+        //練習靜態方法
+        //使用
+        //Application.OpenURL; 
+
+    }
+
+    private void Update()
+    {
+        //靜態取得
+        print("是否輸入任意鍵:" + Input.anyKeyDown); 
+        print("經過時間:" + Time.time); 
     }
 }
