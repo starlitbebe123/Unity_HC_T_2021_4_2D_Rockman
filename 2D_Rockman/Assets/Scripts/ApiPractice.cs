f1d333d64f05258fb5764f3908e25d7db99d7893
starlitbebe123 <82298568+starlitbebe123@users.noreply.github.com>
2021-05-09 17:09:07 +0800

    unity

---
+++ 2D_Rockman/Assets/Scripts/ApiPractice.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class ApiPractice : MonoBehaviour
+{
+    //非靜態API：需要物件
+    //定義一個欄位-物件
+
+    public Camera cam;
+    public SpriteRenderer sprt;
+    public Transform sprt2;
+    public Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam.depth = Camera.main.depth;
+        sprt.color = Color.black;
+        cam.backgroundColor = new Color(1, 0, 0);
+        sprt.flipY = true;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //非靜態方法
+        sprt2.Rotate(0, 0, 1);
+        rig.AddForce(new Vector2(0, 10));
+    }
+}
--- 2D_Rockman/Assets/Scripts/Player.cs
+++ 2D_Rockman/Assets/Scripts/Player.cs
@@ -17,7 +17,12 @@ public class Player : MonoBehaviour
 
     [Header("是否在地板上")]
     [Tooltip("判斷是否在地板上")]
-    public bool onGround = false;
+    public bool Grounded = false;
+
+    [Header("判斷地板碰撞的位移與半徑")]
+    [Tooltip("判斷是否在地板上")]
+    public Vector3 groundOffset;
+    public float groundRadius = 0.2f;
 
     [Header("子彈(遊戲物件)")]
     [Tooltip("子彈(遊戲物件)")]
@@ -39,18 +44,64 @@ public class Player : MonoBehaviour
     Animator ani;
     #endregion
 
-    #region 方法
+    //按右上...的Debug可以看到private的
+
+    #region 事件
+    private void Start()
+    {
+        //利用程式取得元件
+        //傳回元件 取得元件() - <泛型>
+        rig = GetComponent<Rigidbody2D>(); 
+
+    }
+    //一秒約執行60次
+    private void Update()
+    {
+        Movement();
+        Jump();
+    }
+
+    //繪製圖示 - 輔助編輯時的圖形線條
+    private void OnDrawGizmos()
+    {
+        //繪製地板監控
+        //1.指定顏色
+        Gizmos.color = new Color(1,0,0,0.5f);
+        //2.繪製圖形
+        //transform可以抓到此腳本同一層的變形元件
+        Gizmos.DrawSphere(transform.position + groundOffset, groundRadius); 
+    }
 
     private void Movement()
     {
-
+        //1.要抓到玩家按下左右鍵的資訊Input
+        //2.使用左右鍵的資訊控制腳色移動
+        //水平移動，按左鍵會是-1，按右鍵會是1，不按是0
+        float h = Input.GetAxis("Horizontal");
+        //鋼體.加速度 = 二維向量(水平 * 速度 * 一幀的時間, 0); 
+        //一幀的時間 - 解決不同效能的裝置速度差問題, 指定回原本的Y軸加速度)
+        rig.velocity = new Vector2(h * playerSpeed * Time.deltaTime, rig.velocity.y); 
     }
+    #region 方法
     /// <summary>
     /// 跳越
     /// </summary>
     private void Jump()
     {
+        //如果 玩家 按下空白鍵 就 往上跳躍
+        //判斷式C#
+        //if(布林值){當布林值等於true時會執行這個程式}
+        if (Grounded)
+        {
 
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                print("跳躍");
+                //鋼體.添加推力(二維向量);
+                rig.AddForce(new Vector2(0, 300));
+            }
+        }
     }
 
     /// <summary>
@@ -87,5 +138,6 @@ public class Player : MonoBehaviour
     {
 
     }
+    #endregion
 }
 #endregion
